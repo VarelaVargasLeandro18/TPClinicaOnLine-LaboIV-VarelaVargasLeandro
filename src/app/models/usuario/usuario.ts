a904762cbe241d5518664fc74d9909084e626b81
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-10-16 20:30:08 -0300

    Agregados atributos

--- src/app/models/usuario/usuario.ts
+++ src/app/models/usuario/usuario.ts
@@ -1,11 +1,14 @@
 export class Usuario {
-
     // EL USUARIO NO DEBERÍA TENER QUE TENER ESTE ERROR, PERO NO LE ENCONTRÉ ALTERNATIVA.
     constructor(
+        public nombre? : string,
+        public apellido? : string,
+        public edad? : number,
+        public dni? : number,
+        public obraSocial? : string,
         public email? : string,
         public contrasenia? : string,
-        public nombre? : string,
-        public apellido? : string
+        public imagenUnoUrl? : string,
+        public imagenDosUrl? : string
     ) {}
-
 }
