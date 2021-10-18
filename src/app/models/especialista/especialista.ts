66154aaf3ae168e9dacad409e185a07146324cff
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-10-17 23:09:51 -0300

    Creado modelo de especialista

---
+++ src/app/models/especialista/especialista.ts
@@ -0,0 +1,14 @@
+export class Especialista {
+
+    constructor(
+        public nombre? : string,
+        public apellido? : string,
+        public edad? : number,
+        public dni? : number,
+        public especialidad? : string,
+        public email? : string,
+        public password? : string,
+        public imagenUnoUrl? : string
+    ) {}
+
+}
