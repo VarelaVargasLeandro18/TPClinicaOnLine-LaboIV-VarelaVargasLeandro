cafbb87517f01214eb2b63bbfe472308b807f749
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-11-04 03:11:35 -0300

    Agregada propiedad recaptcha a environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,8 @@ export const environment = {
     storageBucket: "labo-iv-tpclinica-lvv.appspot.com",
     messagingSenderId: "927885202294",
     appId: "1:927885202294:web:0c7302c9b03e525078e057"
+  },
+  recaptcha: {
+    siteKey: '6LcTjP0cAAAAAGDqanxLPJagGfiC7u2KyP1xynHD'
   }
 };
